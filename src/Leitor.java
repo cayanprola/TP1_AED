0b45814c117f587d6ba2cb611935a7ebfef1f50f
Cayan <cayanprola@gmail.com>
2023-05-05 10:04:45 +0100

    All data inside the hash maps

--- src/Leitor.java
+++ src/Leitor.java
@@ -1,57 +1,52 @@
 import java.io.*;
 import java.util.*;
 
-public class Leitor<K,V> extends HashMap<K,V> {
-    public static void lerFile(ArrayList<String> arr1, ArrayList<String> arr2, ArrayList<HashMap<?, ?>> arrMap) throws Exception {
+public class Leitor<K, V> extends HashMap<K, V> {
+    public static void lerFile(ArrayList<String[]> arr1, ArrayList<String[]> arr2, ArrayList<HashMap<?, ?>> arrMap) throws Exception {
+
+        String line = "";
+        String splitBy = ",";
 
         ArrayList<HashMap<?, ?>> myList = new ArrayList<HashMap<?, ?>>();
+        HashMap<Integer, String> data1 = new HashMap<Integer, String>();
+        HashMap<Integer, String> data2 = new HashMap<Integer, String>();
 
-        HashMap<String, String> data1 = new HashMap<String, String>();
-        HashMap<String, String> data2 = new HashMap<String, String>();
-        HashMap<String, Integer> dataIndex1 = new HashMap<String, Integer>();
-        HashMap<String, Integer> dataIndex2 = new HashMap<String, Integer>();
-
-
-        Scanner scFile1 = new Scanner(new File("./files/rooms/booking.csv"));
-        Scanner scFile2 = new Scanner(new File("./files/rooms/expedia.csv"));
+        //ler string do expedia com oq esta entre aspas, e nao usar somente split por virgula
 
 
-        scFile1.useDelimiter(",");
-        scFile2.useDelimiter(",");
+        try {
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader booking = new BufferedReader(new FileReader("./files/rooms/booking.csv"));
+            BufferedReader expedia = new BufferedReader(new FileReader("./files/rooms/expedia.csv"));
 
-        int i = 0;
-        while (scFile1.hasNextInt()) {
-            dataIndex1.put("index", scFile1.nextInt());
-//            data1.put("name", scFile1.next());
-            i++;
-            arr1.add(scFile1.next());
-        }
-        for (String x : arr1) {
-//            System.out.println(x);
-        }
-        scFile1.close();
-        while (scFile2.hasNextInt()) {
-            int j = 0;
-            dataIndex2.put("index", scFile2.nextInt());
-//            data2.put("name", scFile2.next());
+            while ((line = booking.readLine()) != null)   //returns a Boolean value
+            {
+                arr1.add(line.split(splitBy));
+            }
+            while ((line = expedia.readLine()) != null)   //returns a Boolean value
+            {
+                arr2.add(line.split(splitBy));    // use comma as separator
+            }
+            for (int x = 0; x < arr1.size(); x++) {
+                data1.put(x, Arrays.toString(arr1.get(x)));
+            }
+            for (int x = 0; x < arr1.size(); x++) {
+                System.out.println(data1.get(x));
+            }
+            for (int x = 0; x < arr2.size(); x++) {
+                data2.put(x, Arrays.toString(arr2.get(x)));
+            }
+            for (int x = 0; x < arr2.size(); x++) {
+                System.out.println(data2.get(x));
+            }
+            
+            myList.add(data1);
+            myList.add(data2);
 
-            j++;
-            arr2.add(scFile2.next());
-        }
-        for (String x : arr2) {
-//            System.out.println(x);
-        }
-        scFile2.close();
-
-        myList.add(data1);
-        myList.add(dataIndex1);
-        myList.add(data2);
-        myList.add(dataIndex2);
-
-        for(i = 0; i < arr1.size(); i++) {
-            System.out.println(dataIndex1.get("index"));
+            booking.close();
+            expedia.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main<K,V> extends HashMap<K, V>{
     public static void main(String[] args) throws Exception {
         ArrayList<HashMap<?, ?>> myList = new ArrayList<HashMap<?, ?>>();
 
-        ArrayList<String> arr1 = new ArrayList<String>();
-        ArrayList<String> arr2 = new ArrayList<String>();
+        ArrayList<String[]> arr1 = new ArrayList<String[]>();
+        ArrayList<String[]> arr2 = new ArrayList<String[]>();
         Leitor l = new Leitor();
         l.lerFile(arr1, arr2, myList);
     }
